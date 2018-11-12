1e54a35d4d2b62e98e188c4ae1db3415c55ab746
Carlos Duarte <carlosivanduarte@gmail.com>
2018-11-12 08:56:48 -0500

    First commit

---
+++ alkanza-spring/src/main/java/com/alkanza/distance/AlkanzaSpringApplication.java
@@ -0,0 +1,12 @@
+package com.alkanza.distance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkanzaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkanzaSpringApplication.class, args);
+	}
+}
